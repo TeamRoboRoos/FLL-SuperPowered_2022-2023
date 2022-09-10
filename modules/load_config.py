0280712322d7b88205b217500bd9c96c2b50b6c8
Alan Lu <alan.lu@roboroos.org.au>
2022-09-10 15:44:25 +0930

    Updated load config
    Changed names for two robots
    Artemis and Apollo
    
    Added condition for when no robot config is found

--- modules/load_config.py
+++ modules/load_config.py
@@ -28,13 +28,13 @@ class config:
         self.runButton = None
         self.leftButton = None
 
-        if self.name == "ev3dev":
+        if self.name == "artemis":
             self.SPEED_LIST_COUNT = 2000
             self.ACCELERATION = 250
             self.STARTSPEED = 50
             self.TURN_SPEED_MIN = 20
             self.TURN_SPEED_MAX = 180
-            self.LIGHTCAL_CONF = "ev3dev.cal"
+            self.LIGHTCAL_CONF = "artemis.cal"
 
             self.Lmotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
             self.Rmotor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
@@ -59,3 +59,40 @@ class config:
             # self.xlift = forklift(Motor(Port.B))
             # self.ylift = forklift(Motor(Port.C))
             # self.lift = doubleForklift(self.xlift, self.ylift)
+
+        elif self.name == "apollo":
+            self.SPEED_LIST_COUNT = 2000
+            self.ACCELERATION = 250
+            self.STARTSPEED = 50
+            self.TURN_SPEED_MIN = 20
+            self.TURN_SPEED_MAX = 180
+            self.LIGHTCAL_CONF = "apollo.cal"
+
+            self.Lmotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+            self.Rmotor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+            self.LMmotor = Motor(Port.B)
+            self.RMmotor = Motor(Port.D)
+
+            # self.runButton = runButton(TouchSensor(Port))
+            self.gyro = GyroSensor(Port.S1, Direction.COUNTERCLOCKWISE)
+            self.Llight = lightSensor(ColorSensor(Port.S3))
+            self.Rlight = lightSensor(ColorSensor(Port.S4))
+
+            self.lift = forklift(self, self.RMmotor, 11, 40, 8)
+            self.drive = driveBase(self, DriveBase(self.Lmotor, self.Rmotor, 56, 104),
+                                   self.Lmotor, self.Rmotor, self.gyro, self.runButton, Llight=self.Llight, Rlight=self.Rlight)
+
+            self.menu = {
+                "runs": [["run1", "run2", "run3"], [run1(self), run2(self), run3(self)]],
+                "utility": [["lightCal", "gyrodrift", "tyreClean"], [self.drive.lightCal, self.drive.gyroDrift, self.drive.tyreClean]],
+                "pages": ["runs", "utility"]
+            }
+
+            # self.xlift = forklift(Motor(Port.B))
+            # self.ylift = forklift(Motor(Port.C))
+            # self.lift = doubleForklift(self.xlift, self.ylift)
+        else:
+            self.ev3.screen.clear()
+            self.ev3.screen.print("Unkown robot\nNo config found")
+            while True:
+                wait(100)
