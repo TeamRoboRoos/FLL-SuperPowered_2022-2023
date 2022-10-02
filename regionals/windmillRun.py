a39d5f18c9b3e390c0199356d8d5f7900d546d4a
Alan Lu <alan.lu@roboroos.org.au>
2022-10-02 17:00:33 +1030

    Continued to work on windmill run
    Working on car atm

--- regionals/windmillRun.py
+++ regionals/windmillRun.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from pybricks.tools import wait
 
 
 class windmillRun(Thread):
@@ -8,10 +9,25 @@ class windmillRun(Thread):
         self.flipper = config.RMmotor
 
     def run(self):
+        self.drive.setHead()
+
         self.drive.moveDist(580, heading=0)
-        self.drive.moveDost(-60, heading=0)
-        self.drive.turnTo(-40)
+        self.drive.moveDist(-80, heading=0, turn=False)
+        self.drive.turnTo(-45)
         self.drive.moveLight(self.config.Rlight, [0, 10], heading=-40)
+        self.drive.moveDist(140, heading=-40)
         self.drive.turnTo(45)
+
+        for i in range(0, 3):
+            self.drive.moveDist(80, heading=45)
+            wait(500)
+            self.drive.moveDist(-50, heading=45)
+            wait(800)
+
+        self.drive.moveDist(-190)
+        self.drive.turnTo(-45)
+        self.drive.moveDist(290, heading=-45)
+        self.flipper.run_angle(400, 500)
+        self.flipper.stop()
 
         self.config.state.setState(1)
