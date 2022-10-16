932348e4695dd03b1f9c77f853a1d211669780e9
Alan Lu <alan.lu@roboroos.org.au>
2022-10-16 16:03:00 +1030

    Continued to work on windmill run
    changed wait to timer.wait

--- regionals/windmillRun.py
+++ regionals/windmillRun.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from pybricks.tools import wait
 
 
 class windmillRun(Thread):
@@ -7,27 +6,38 @@ class windmillRun(Thread):
         self.config = config
         self.drive = config.drive
         self.flipper = config.RMmotor
+        self.wait = config.timer.wait
 
     def run(self):
         self.drive.setHead()
 
-        self.drive.moveDist(580, heading=0)
+        self.drive.moveDist(590, heading=0)
         self.drive.moveDist(-80, heading=0, turn=False)
         self.drive.turnTo(-45)
         self.drive.moveLight(self.config.Rlight, [0, 10], heading=-40)
         self.drive.moveDist(140, heading=-40)
         self.drive.turnTo(45)
 
-        for i in range(0, 3):
-            self.drive.moveDist(80, heading=45)
-            wait(500)
-            self.drive.moveDist(-50, heading=45)
-            wait(800)
+        self.drive.moveDist(80, heading=45)
+        self.wait(500)
+        self.drive.moveDist(-50, heading=45)
+        self.wait(800)
+        self.drive.moveDist(80, heading=45)
+        self.wait(500)
+        self.drive.moveDist(-50, heading=45)
+        self.wait(800)
+        self.drive.moveDist(80, heading=45)
+        self.wait(500)
+        self.drive.moveDist(-50, heading=45)
+        self.drive.setHead(45)
 
-        self.drive.moveDist(-190)
+        self.drive.moveDist(-170)
         self.drive.turnTo(-45)
-        self.drive.moveDist(290, heading=-45)
-        self.flipper.run_angle(400, 500)
+        self.drive.moveDist(293, heading=-45)
+        self.flipper.run_angle(900, 530)
         self.flipper.stop()
+        self.flipper.run_angle(-900, 530)
+        self.flipper.stop()
+        self.drive.moveDist(-1100)
 
         self.config.state.setState(1)
