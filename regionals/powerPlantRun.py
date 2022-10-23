7ea27521172cdb1c40a2b942d7a52c2bd8402170
Alan Lu <alan.lu@roboroos.org.au>
2022-10-23 14:42:21 +1030

    Adjusted powerPlantRun

--- regionals/powerPlantRun.py
+++ regionals/powerPlantRun.py
@@ -9,9 +9,9 @@ class powerPlantRun(Thread):
 
     def run(self):
         self.drive.setHead(-90)
-        self.drive.moveDist(660, 600, heading=-90)
+        self.drive.moveDist(665, 600, heading=-90)
         self.arm.run_angle(-200, 200)
-        self.drive.moveDist(-700, 1000, down=False)
+        self.drive.moveDist(-700, 1000, heading=-95, turn=False, down=False)
         self.arm.stop()
 
         self.config.state.setState(1)
