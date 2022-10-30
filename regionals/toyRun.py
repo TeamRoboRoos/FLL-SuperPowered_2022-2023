e67ffeb364011eb767b7a8e500a2e9e1a17bc65d
Alan Lu <alan.lu@roboroos.org.au>
2022-10-30 16:31:04 +1030

    Started toyRun
    Full program created
    
    Needs tweaking and testing

--- regionals/toyRun.py
+++ regionals/toyRun.py
@@ -8,4 +8,33 @@ class toyRun(Thread):
         self.wait = config.timer.wait
 
     def run(self):
+        self.drive.setHead()
+
+        self.drive.moveDist(120, heading=0)
+        self.drive.moveLight(self.config.Llight, [0, 10], heading=0)
+
+        self.drive.turnTo(25)
+        self.drive.moveDist(620)
+        self.drive.turnTo(0)
+
+        self.drive.moveDist(200, heading=0)
+        self.drive.setHead(-5)
+
+        self.drive.moveDist(-200, heading=0)
+        self.drive.turnTo(90)
+        self.drive.moveDist(300, heading=90)
+        self.drive.moveLight(self.config.Rlight, [0, 10], heading=90)
+        self.drive.moveDist(270, heading=90)
+        self.drive.turnTo(130)
+
+        self.drive.moveDist(80, heading=135)
+
+        self.drive.moveDist(-100)
+        self.drive.turnTo(90)
+        self.drive.moveDist(400)
+        self.drive.turnTo(180)
+
+        self.drive.moveDist(-150)
+        self.drive.turnTo(160)
+
         self.config.state.setState(1)
