d438a09644fb554c8f286ba91d5ab715b4e31423
Alan Lu <alan.lu@roboroos.org.au>
2022-10-30 16:30:22 +1030

    Modified oilRun
    Now moves forwards slightely further
    
    Increases chances of mechanism activating

--- regionals/oilRun.py
+++ regionals/oilRun.py
@@ -10,7 +10,7 @@ class oilRun(Thread):
     def run(self):
         self.drive.setHead()
 
-        self.drive.moveDist(200)
+        self.drive.moveDist(220)
         self.drive.moveDist(-400)
 
         self.config.state.setState(1)
