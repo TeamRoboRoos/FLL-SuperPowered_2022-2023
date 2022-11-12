6bb19f6a54d035e2135b2bf55dd4ebec4e701bc7
Alan Lu <alan.lu@roboroos.org.au>
2022-11-12 16:35:20 +1030

    Tweaked winmillRun

--- regionals/windmillRun.py
+++ regionals/windmillRun.py
@@ -12,7 +12,7 @@ class windmillRun(Thread):
         self.drive.setHead()
 
         self.drive.moveDist(590, heading=0)  # Hits the TV
-        self.drive.moveDist(-80, heading=0, turn=False)
+        self.drive.moveDist(-100, heading=0, turn=False)
         self.drive.spinTo(-45)
         self.drive.moveLight(self.config.Rlight, [0, 10], heading=-40)
         self.drive.moveDist(140, heading=-40)
@@ -36,12 +36,12 @@ class windmillRun(Thread):
 
         # In place for the car
         self.drive.moveDist(293, heading=-45)
-        self.flipper.run_angle(900, 450)
+        self.flipper.run_angle(800, 450)
         self.flipper.stop()
         # self.flipper.run_angle(-900, 450)
         self.flipper.stop()
 
         # Grabs the rechargable battery and goes home
-        self.drive.moveDist(-1100, heading=-45)
+        self.drive.moveDist(-1100, down=False, heading=-45)
 
         self.config.state.setState(1)
