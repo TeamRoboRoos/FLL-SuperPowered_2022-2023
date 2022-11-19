c62cddb7dbf46e82352d8f9bbe96851045e8c9a6
Alan Lu <alan.lu@roboroos.org.au>
2022-11-20 09:43:18 +1030

    Made arm go back after powerplantRun

--- regionals/powerPlantRun.py
+++ regionals/powerPlantRun.py
@@ -11,6 +11,7 @@ class powerPlantRun(Thread):
         self.drive.setHead(-90)
         self.drive.moveDist(660, 600, heading=-92)
         self.arm.run_angle(-400, 600)
+        Thread(target=self.arm.run_angle, args=[400, 600]).start()
         self.drive.moveDist(-700, 1000, heading=-95, turn=False, down=False)
         self.arm.stop()
 
