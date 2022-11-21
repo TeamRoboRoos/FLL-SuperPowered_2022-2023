0d14ad967a31c695538cc2fb0452d294f89f23e0
Alan Lu <alan.lu@roboroos.org.au>
2022-11-21 14:36:20 +1030

    Made powerPlantRun more consistent

--- regionals/powerPlantRun.py
+++ regionals/powerPlantRun.py
@@ -9,9 +9,10 @@ class powerPlantRun(Thread):
 
     def run(self):
         self.drive.setHead(-90)
-        self.drive.moveDist(660, 600, heading=-92)
-        self.arm.run_angle(-400, 600)
-        Thread(target=self.arm.run_angle, args=[400, 600]).start()
+        self.drive.moveDist(640, 600, heading=-92)
+        self.arm.run_angle(-400, 300)
+        self.drive.moveDist(20, heading=-92)
+        Thread(target=self.arm.run_angle, args=[400, 300]).start()
         self.drive.moveDist(-700, 1000, heading=-95, turn=False, down=False)
         self.arm.stop()
 
