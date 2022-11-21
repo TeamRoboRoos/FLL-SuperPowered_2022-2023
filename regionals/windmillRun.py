eec9390bb8b0b281416311db0fb9393a6a33304a
Alan Lu <alan.lu@roboroos.org.au>
2022-11-21 17:16:03 +1030

    Adjusted windmillRun for using the jig

--- regionals/windmillRun.py
+++ regionals/windmillRun.py
@@ -11,19 +11,19 @@ class windmillRun(Thread):
     def run(self):
         self.drive.setHead()
 
-        self.drive.moveDist(590, heading=0)  # Hits the TV
+        self.drive.moveDist(610, speed=300, heading=0)  # Hits the TV
         self.drive.moveDist(-100, heading=0, turn=False)
-        self.drive.spinTo(-45)
-        self.drive.moveLight(self.config.Rlight, [0, 10], heading=-45)
+        self.drive.spinTo(-43)
+        self.drive.moveLight(self.config.Rlight, [0, 10], heading=-43)
         self.drive.moveDist(140, heading=-40)
         self.drive.spinTo(45)
 
         # Push the windmill three times
-        self.drive.moveDist(80)
+        self.drive.moveDist(100)
         self.drive.moveDist(-50)
-        self.drive.moveDist(80)
+        self.drive.moveDist(100)
         self.drive.moveDist(-50)
-        self.drive.moveDist(80)
+        self.drive.moveDist(100)
         self.drive.setHead(45)
 
         self.drive.moveDist(-230, heading=45)
@@ -32,12 +32,12 @@ class windmillRun(Thread):
         # In place for the car
         self.drive.moveDist(293, heading=-45)
         # self.flipper.run_angle(800, 450)
-        self.flipper.run_time(800, 900)
+        self.flipper.run_time(500, 900)
         # self.flipper.run_angle(-900, 450)
-        Thread(target=self.flipper.run_time, args=[-400, 900]).start()
+        Thread(target=self.flipper.run_time, args=[-300, 1400]).start()
 
         # Grabs the rechargable battery and goes home
-        self.drive.moveDist(-1100, down=False, heading=-45)
+        self.drive.moveDist(-1100, down=False, heading=-50)
         self.flipper.stop()
 
         self.config.state.setState(1)
