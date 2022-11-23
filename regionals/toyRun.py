e4701605fce6c8f0f12066d962de0f8ce13bf7c5
Alan Lu <alan.lu@roboroos.org.au>
2022-11-23 20:14:53 +1030

    Toyrun drops off two water units

--- regionals/toyRun.py
+++ regionals/toyRun.py
@@ -5,6 +5,7 @@ class toyRun(Thread):
     def __init__(self, config):
         self.config = config
         self.drive = config.drive
+        self.arm = config.RMmotor
         self.wait = config.timer.wait
 
     def run(self):
@@ -20,6 +21,11 @@ class toyRun(Thread):
 
         self.drive.moveDist(-220, heading=0)
         self.drive.spinTo(90)
+
+        self.drive.lineFollower(distance=50, mode=1,
+                                speed=120, kp=0.3, ki=0, kd=0)
+
+        self.arm.run_angle(-600, 200)
 
         self.drive.lineFollower(mode=1, speed=120, kp=0.3, ki=0, kd=0)
         self.drive.setHead(90)
